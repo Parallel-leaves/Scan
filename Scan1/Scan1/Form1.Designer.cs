4527463394d93f09d2d88bd9c4f195b9d01c0ff9
Parallel-leaves <80300375+Parallel-leaves@users.noreply.github.com>
2021-11-22 23:16:00 +0800

    Add files via upload

---
+++ Scan1/Scan1/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace Scan1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(27, 34);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(740, 289);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(606, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "开始扫描";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 330);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "主机地址";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 330);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(123, 25);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 330);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "开始端口";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(345, 330);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(129, 25);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(264, 375);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "结束端口";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(345, 375);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(129, 25);
+            this.textBox3.TabIndex = 7;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(540, 331);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(226, 23);
+            this.progressBar1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(540, 361);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 15);
+            this.label4.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(711, 361);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 15);
+            this.label5.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(640, 360);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 15);
+            this.label6.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Scan2/Scan2/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Scan2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //自定义变量
+        private int port;//记录当前扫描的端口号
+        private string Address;//记录扫描的系统地址
+        private bool[] done = new bool[65536];//记录端口是否已经扫描
+        private int start;//记录扫描的起始端口
+        private int end;//记录扫描的结束端口
+        private bool OK;
+        private Thread scanThread; 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = textBox2.Text;//设定进度条的起始端口
+            label5.Text = textBox3.Text;//设置进度条的终止端口
+            //创建线程
+            Thread procss = new Thread(new ThreadStart(PortScan));
+            procss.Start();
+            //设定进度条的范围
+            progressBar1.Minimum = Int32.Parse(textBox2.Text);
+            progressBar1.Maximum = Int32.Parse(textBox3.Text);
+            //显示框的初始化
+            listBox1.Items.Clear();
+            listBox1.Items.Add("端口扫描器 v1.0");
+            listBox1.Items.Add(" ");
+        }
+        private void PortScan()
+        {
+            start = Int32.Parse(textBox2.Text);
+            end = Int32.Parse(textBox3.Text);
+            //检查端口的合法性
+            if ((start >= 0 && start <= 65536) && (end >= 0 && end <= 65536) && (start <= end))
+            {
+
+                Invoke(new Action(() => {//在线程里修改界面
+                    listBox1.Items.Add("开始扫描：这个过程可能需要等待几分钟！");
+                }));
+                Address = textBox1.Text;
+                for (int i = start; i <= end; i++)
+                {
+                    port = i;
+                    //对该端口进行扫描的线程
+                    scanThread = new Thread(Scan);
+                    scanThread.Start();
+                    //使线程睡眠
+                    System.Threading.Thread.Sleep(100);
+
+                    Invoke(new Action(() => {//在线程里修改界面
+                        progressBar1.Value = i;
+                        label6.Text = i.ToString();
+                    }));
+                }
+                //未完成时情况
+                while (!OK)
+                {
+                    OK = true;
+                    for (int i = start; i <= end; i++)
+                    {
+                        if (!done[i])
+                        {
+                            OK = false;
+                            break;
+                        }
+                    }
+                }
+
+                Invoke(new Action(() => {//在线程里修改界面
+                    listBox1.Items.Add("扫描结束！");
+                }));
+                System.Threading.Thread.Sleep(1000);
+            }
+            else
+            {
+                Invoke(new Action(() => {//在线程里修改界面
+                    MessageBox.Show("输入错误，端口范围为[0,65536]");
+                }));
+
+            }
+        }
+        private void Scan()
+        {
+            int portnow = port;
+            //创建线程变量
+            Thread Threadnow = scanThread;
+            done[portnow] = true;
+            //创建TcpClient对象，TcpClient用于TCP网络服务提供客户端连接
+            TcpClient objTCP = null;
+            //扫描端口，成功就写入信息
+            try
+            {
+                objTCP = new TcpClient(Address, portnow);
+                Invoke(new Action(() => {//在线程里修改界面
+                    listBox1.Items.Add("端口" + portnow.ToString() + "开放！");
+                }));
+                objTCP.Close();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
